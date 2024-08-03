5692b46caeba96ed2611de8dc1382dcc4a25551e
dlarjsgml321 <dlarjsgml321@naver.com>
2024-08-04 00:14:07 +0900

    홈페이지 html, controller

---
+++ src/main/java/com/itwill/finalproject/web/HomeController.java
@@ -0,0 +1,20 @@
+package com.itwill.finalproject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		log.info("home()");
+		
+		return "home";
+	}
+	
+
+}
