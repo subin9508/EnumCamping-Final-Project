566d2b4bdd988d6fd7d243be70ff7c85ac0ae298
ADMIN <ADMIN@DESKTOP-JDFSTM9>
2024-08-08 15:24:51 +0900

    240808 (소개, 배치도 페이지 부트 수정)

---
+++ src/main/java/com/itwill/finalproject/web/IntroduceController.java
@@ -0,0 +1,35 @@
+package com.itwill.finalproject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/introduce")
+public class IntroduceController {
+	
+	@GetMapping("/map")
+	public String map() {
+		log.debug("map()");
+		return "/introduce/map";
+	
+	}
+	
+	@GetMapping("/enumIntro")
+	public String enumIntro() {
+		log.debug("enumIntro()");
+		
+		return "/introduce/enumIntro";
+	}
+	
+	@GetMapping("/facilityLayout")
+	public String facilityLayout() {
+		log.debug("facilityLayout()");
+		
+		return "/introduce/facilityLayout";
+	}
+
+}
