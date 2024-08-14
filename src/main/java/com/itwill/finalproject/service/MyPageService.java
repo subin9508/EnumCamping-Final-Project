dd9bd191a73a3e0aeeec93718e5d3df7a7c8fb0c
woodstock0513 <judydayoung@gmail.com>
2024-08-14 13:47:53 +0900

    0814

--- src/main/java/com/itwill/finalproject/service/MyPageService.java
+++ src/main/java/com/itwill/finalproject/service/MyPageService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.itwill.finalproject.domain.User;
 import com.itwill.finalproject.dto.UserUpdateDto;
+import com.itwill.finalproject.repository.ReservationMasterRepository;
 import com.itwill.finalproject.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MyPageService {
 	
 	private final UserRepository userRepo;
+	private final ReservationMasterRepository resRepo;
 	
 	
 	// 사용자 정보 조회
