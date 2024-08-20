9f55bb06d0874341a5dd998012bf363bea5f1390
katekim <kms252502@naver.com>
2024-08-20 23:05:26 +0900

    탈퇴 수정중

--- src/main/java/com/itwill/finalproject/config/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/itwill/finalproject/config/CustomUsernamePasswordAuthenticationFilter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //package com.itwill.finalproject.config;
 //
 //import org.springframework.security.authentication.BadCredentialsException;
@@ -48,60 +47,3 @@
 //        return authentication;
 //    }
 //}
-=======
-package com.itwill.finalproject.config;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.itwill.finalproject.domain.User;
-import com.itwill.finalproject.domain.UserRole;
-import com.itwill.finalproject.service.UserService;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-    private final UserService userService;
-
-    public CustomUsernamePasswordAuthenticationFilter(AuthenticationManager authenticationManager, UserService userService) {
-        super(authenticationManager);
-        this.userService = userService;
-    }
-
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        String username = obtainUsername(request);
-        String password = obtainPassword(request);
-
-        if (username == null) {
-            username = "";
-        }
-
-        if (password == null) {
-            password = "";
-        }
-
-        username = username.trim();
-
-        User user = userService.findByUserId(username);
-        if (user != null) {
-            UserRole userRole = UserRole.fromValue(user.getUserRole());
-            if (userRole == UserRole.WITHDRAWUSER) {
-                throw new BadCredentialsException("Withdrawn users cannot log in.");
-            }
-        }
-
-        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
-
-        setDetails(request, authRequest);
-
-        return this.getAuthenticationManager().authenticate(authRequest);
-    }
-}
->>>>>>> branch 'feature15' of https://github.com/woodstock0513/enum_final_project.git
