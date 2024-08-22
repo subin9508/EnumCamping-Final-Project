66eccc4c4fe5bbbf5264335f1154db2ca79e0a7c
woodstock0513 <judydayoung@gmail.com>
2024-08-22 18:18:14 +0900

    0822 수정된 input 체크

--- src/main/resources/static/js/price.js
+++ src/main/resources/static/js/price.js
@@ -5,6 +5,21 @@
 
  document.addEventListener('DOMContentLoaded',()=>{
     
+    
+    
+    const inputs = document.querySelectorAll('#modifyForm input[type="text"]');
+    const modifiedInputs = new Set(); // 수정된 input 요소들을 저장하는 Set
+
+    inputs.forEach(input => {
+        input.addEventListener('input', (event) => {
+            //change는// 사용자가 input을 수정한 경우, 포커스 이동을 해야 적용됨
+            // 사용자가 input을 수정하는 즉시 호출됨
+            modifiedInputs.add(event.target);
+            event.target.style.backgroundColor = '#e0f7fa'; // 수정된 요소에 배경색을 추가 (체크용)
+        });
+    });
+    
+    
     const btnApply = document.querySelector('button#btnApply');
     const modifyForm = document.querySelector('#modifyForm');
     
