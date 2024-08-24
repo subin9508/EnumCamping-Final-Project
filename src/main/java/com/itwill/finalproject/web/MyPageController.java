a2ca86461b96a6049b45ffa157ffe2e59510817a
woodstock0513 <judydayoung@gmail.com>
2024-08-24 13:59:52 +0900

    0824 mypagecontroller import List 추가

--- src/main/java/com/itwill/finalproject/web/MyPageController.java
+++ src/main/java/com/itwill/finalproject/web/MyPageController.java
@@ -1,12 +1,12 @@
 package com.itwill.finalproject.web;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -15,24 +15,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
 import org.springframework.web.bind.annotation.RequestBody;
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.itwill.finalproject.domain.ReservationMaster;
-import com.itwill.finalproject.dto.ReservationDetailDto;
+
 import com.itwill.finalproject.domain.QnA;
+import com.itwill.finalproject.domain.ReservationMaster;
 import com.itwill.finalproject.domain.User;
 import com.itwill.finalproject.dto.QnAListItemDto;
 import com.itwill.finalproject.dto.QnAUpdateDto;
+import com.itwill.finalproject.dto.ReservationDetailDto;
 import com.itwill.finalproject.dto.UserUpdateDto;
 import com.itwill.finalproject.service.MyPageService;
 import com.itwill.finalproject.service.QnAService;
-
 import com.itwill.finalproject.service.UserService;
 
 import jakarta.servlet.http.HttpServletResponse;
