705d5d27e273803384cb013a55a87b10c2caee14
woodstock0513 <judydayoung@gmail.com>
2024-08-26 18:11:27 +0900

    0826 퍼센트 인상

--- src/main/resources/static/js/price_zones.js
+++ src/main/resources/static/js/price_zones.js
@@ -6,9 +6,9 @@
  document.addEventListener('DOMContentLoaded',()=>{
     
     
-    
     const inputs = document.querySelectorAll('form.modifyForm input[type="text"]');
     const modifiedInputs = new Set(); // 수정된 input 요소들을 저장하는 Set
+
 
     inputs.forEach(input => {
         input.addEventListener('input', (event) => {
@@ -18,6 +18,25 @@
             event.target.style.backgroundColor = '#e0f7fa'; // 수정된 요소에 배경색을 추가 (체크용)
         });
     });
+    
+    const btnApplyPlus = document.querySelector('button#btnApplyPlus');
+    btnApplyPlus.addEventListener('click', () => {
+        const percent = document.querySelector('input#percent').value.trim(); // 입력된 퍼센트 값 가져오기
+        if (percent === "") {
+            alert("퍼센트 값을 입력해주세요.");
+        } else if (confirm(percent + '%를 인상할까요?')) {
+            // 사용자가 확인 버튼을 클릭한 경우, 해당 URL로 이동
+            window.location.href = `/admin/price/zones/percentupdate?percent=${encodeURIComponent(percent)}`;
+        }
+    });
+        
+    const btnApplyMinus = document.querySelector('button#btnApplyMinus');
+        btnApplyMinus.addEventListener('click', () => {
+        const percent = document.querySelector('input#percent').value;
+            if (confirm(percent +'%를 인하할까요?')) {
+                console.log(percent+'인하');
+            }
+        });
     
     
 	const btnApplyZones = document.querySelector('button#btnApplyZones');
