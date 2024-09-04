6c9beab1c738b076120018a6eea42ff652d2987a
joyous121212 <your.email@example.com>
2024-09-04 18:18:47 +0900

    update_calendar 349번째 줄 response.data.price로 변경

--- src/main/resources/static/js/update_calendar.js
+++ src/main/resources/static/js/update_calendar.js
@@ -346,7 +346,7 @@ var nowDate = new Date();  // @param 전역 변수, 실제 오늘날짜 고정�
     
         axios.get(uri)
             .then(response => {
-                const price = (response.data) 
+                const price = (response.data.price) 
                 document.getElementById(`price_${areaIndex}`).innerText = `${price}원`;
     
             })
