1c46dc9d39cf3fc996709df8e13b57dc41cbdb1d
subin9508 <beronica95@hanmail.net>
2024-12-24 23:10:10 +0900

    20241224 깃 경로 수정 테스트

--- src/main/java/com/itwill/finalproject/FinalprojectApplication.java
+++ src/main/java/com/itwill/finalproject/FinalprojectApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class FinalprojectApplication {
+public class FinalprojectApplication { // 깃 수정 테스트
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalprojectApplication.class, args);
